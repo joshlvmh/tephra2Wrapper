48ecb7815e1877e003123ff7eac72f90cbcde61c
Joshua Measure-Hughes <jm17923@bristol.ac.uk>
2022-01-05 15:16:07 +0000

    extras

---
+++ extras/home/env.sh
@@ -0,0 +1,6 @@
+module load languages/intel/2018-u3
+module load tools/git/
+PATH=$PATH:$HOME/local/bin
+export PATH
+#cd /mnt/storage/home/zs20225/TephraProb/
+
---
+++ extras/home/env1.sh
@@ -0,0 +1,3 @@
+module load apps/matlab/2018a
+umask 0000
+module load languages/intel/2018-u3
