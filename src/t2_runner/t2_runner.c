fdfbe77f72982b887d0142235833ab0f4170e5ab
joshlvmh <jm17923@bristol.ac.uk>
2022-01-25 15:27:46 +0000

    end of bp1

--- src/t2_runner/t2_runner.c
+++ src/t2_runner/t2_runner.c
@@ -4,13 +4,13 @@
 #include <time.h>
 #include <sys/time.h>
 #include <sys/resource.h>
-#include <omp.h>
 #include <mpi.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
 
 #define MASTER 0
-#define LINE_LENGTH 130 // keep this, make automatic pref
+#define LINE_LENGTH 124 //130 // keep this, make automatic pref
 #define NUM_LINES 1450000
 #define VEI_START 2 // make these automatic
 #define VEI_END 7
@@ -19,6 +19,8 @@ void initialise(int* nprocs, int* rank, int* size, double* tic);
 void finalise(double* tic, int* rank);
 void process(int* rank, int* size);
 void checkError(const int err, const char *op, const int rank, const int line);
+int lineCounter(char * filename);
+void die(const char* message, const int line, const char* file);
 
 int main(int argc, char* argv[])
 {
@@ -29,43 +31,17 @@ int main(int argc, char* argv[])
   int    size;
   MPI_Status status;
 
+  char * file = "0.txt";
+  printf("lines %d\n", lineCounter(file));
   initialise(&nprocs, &rank, &size, &tic);
 
-  process(&rank, &size);
+  //process(&rank, &size);
 
-  MPI_Barrier(MPI_COMM_WORLD);
+  //MPI_Barrier(MPI_COMM_WORLD);
   finalise(&tic, &rank);
 
   return EXIT_SUCCESS;
 }
-
-void get_indices(char * line)
-{
-  // NetCDF dimensions:
-  // volcano, run, easting, northing, vei, wind string
-  // 145	  10000  111     111       6      1
-  // 
-  char * pch;
-  pch = strtok (line, " /");
-  char * conf;
-  char * wind;
-  for (int k = 0; k < 15; k++)
-  {
-    if (k == 4) conf = pch;
-    if (k == 9) wind = pch;
-    pch = strtok( NULL, " /");
-  }
-  char * volc;
-  volc = strtok( conf, "_");
-  printf("conf: %s  volc: %s  wind: %s \n", conf, volc, wind);
-    
-  // while (pch != NULL)
-  // {
-  //   printf("%s \n", pch);
-  //   pch = strtok( NULL, " /");
-  // }
-}
-
 
 void process(int* rank, int* size)
 {
@@ -73,29 +49,179 @@ void process(int* rank, int* size)
   char str[20];
   char file[] = ".txt";
   char line[LINE_LENGTH];
+  char message[1024];
 
   printf("Rank: %d, Size: %d\n", *rank, *size);
   sprintf(str, "%d", *rank);
   fp = fopen(strcat(str,file),"r");
   if( fp == NULL ) {
-      fprintf(stderr, "Couldn't open %s: %s\n", str, strerror(errno));
-      exit(1);
+    sprintf(message, "Could not open %s: %s\n", str, strerror(errno));
+    die(message, __LINE__, __FILE__);
   }
-
+/*
   for (int j = VEI_START; j < VEI_END+1; j++)
   {
     for (int i = 0; i < *size; i++) {
       fgets(line, LINE_LENGTH+10, fp);
-      get_indices(line);
-      //printf("%c", line[4]);
-
       
-      //system(line);
-      // deal with out file -> netCDF? pickle?
+      system(line);
+      char * stdout_file = strrchr(line, ' ');
+      char rem[] = "rm ";
+      strcat(rem, stdout_file);
+      system(rem);
+      
     }
     printf("rank %d: VEI%d complete\n", *rank, j);
   }
+*/
 }
+
+int lineCounter(char * filename)
+{
+  int stdout_bk;
+  int pipefd[2];
+  char buf[101];
+  char * token;
+  stdout_bk = dup(fileno(stdout));
+  pipe2(pipefd, 0);
+  dup2(pipefd[1], fileno(stdout));
+
+  char command[50] = "wc -l < ";
+  strcat(command, filename);
+  system(command);
+
+  fflush(stdout);
+  close(pipefd[1]);
+
+  dup2(stdout_bk, fileno(stdout));
+  read(pipefd[0], buf, 100);
+
+  token = strtok(buf, " ");
+  return atoi(token);
+}
+
+typedef struct
+{
+  int lines;
+  int number;
+  char * filename;
+  FILE * fid;
+} vei_file;
+
+void assignLines(int* nprocs, int* rank, int* size, double* tic)
+{
+  int counts[*nprocs];
+  vei_file veis[VEI_END-VEI_START + 1];
+  char message[1024];
+
+  char *conf = getenv("CONF");
+  if (conf == NULL)
+  {
+    die("source ./bin/conf.sh to configure environment.", __LINE__, __FILE__);
+  }
+  char t2_file[] = "t2.txt";
+
+  int total_lines = 0;
+  int j;
+  // array of fps to prevent reopening?
+  for (j = VEI_START; j < 3; j++)//VEI_END+1; j++)
+  {
+    char vei[6];
+    snprintf(vei, 7, "/VEI%d/", j);
+    char *fullfile = (char*) malloc(strlen(conf) + strlen(vei) + strlen(t2_file) + 1);
+    strcpy(fullfile, conf);
+    strcat(fullfile, vei);
+    strcat(fullfile, t2_file);
+    FILE *fid;
+    fid = fopen(fullfile, "r");
+    if( fid == NULL ) {
+        sprintf(message, "Could not open %s: %s\n", fullfile, strerror(errno));
+        die(message, __LINE__, __FILE__);
+    }
+    int index = j - VEI_START;
+    veis[index].filename = (char*) malloc(strlen(conf) + strlen(vei) + strlen(t2_file) + 1);
+    strcpy(veis[index].filename, fullfile);
+    veis[index].fid = fid;
+    veis[index].number= j;
+    veis[index].lines = lineCounter(veis[index].filename);
+    total_lines += veis[index].lines;
+
+    printf("filename: %s\nnumber: %d\nlines: %d\n", veis[index].filename, veis[index].number, veis[index].lines);
+    free(fullfile);
+  }
+  printf("total lines: %d\n", total_lines);
+
+
+
+  total_lines=0;
+  int num_ps = 56;
+  int lines_p_proc[num_ps];
+  int lines_vei[6];
+
+  int i;
+  for(i = 0; i < 6; i++)
+  {
+    lines_vei[i] = 1370000;
+    total_lines += lines_vei[i];
+  }
+  printf("total lines: %d\n", total_lines);
+
+  int lower_bound = (total_lines / 10000) / num_ps;
+  int upper_bound = lower_bound + 1;
+
+  int upper_count = (total_lines/10000) - (lower_bound * num_ps);
+  int lower_count = num_ps - upper_count;
+
+  printf("upper: %d * %d + lower: %d * %d = final: %d\n", upper_count, upper_bound, lower_count, lower_bound, ((upper_count*upper_bound)+(lower_count*lower_bound))*10000);
+
+  for (i = 0; i < num_ps; i++)
+  {
+    lines_p_proc[i] = (i < lower_count) ? lower_bound : upper_bound;
+    lines_p_proc[i] *= 10000;
+ //   printf("%d: %d\n", i, lines_p_proc[i]);
+  }
+
+  // for vei file
+  // i = 0 
+  // lnies_p_proc[i] check against, once complete 10000* value, increment i, open 0.txt file based on i
+  //
+
+  int k;
+  int current_rank = 0;
+  int lines_done = 0;
+  char line[LINE_LENGTH];
+  for (j = VEI_START; j < VEI_END+1; j++)
+  {
+    if (veis[j-VEI_START].fid == NULL) printf("null fid on line %d\n", __LINE__);
+    for (k = 0; k < veis[j-VEI_START].lines; k++)
+    {
+      fgets(line, LINE_LENGTH+10, veis[j-VEI_START].fid);
+      char * cur_str;
+      sprintf(cur_str, "%d", current_rank);
+      FILE* r_file = fopen(strcat(cur_str, ".txt"), "a");
+      if( r_file == NULL ) {
+        sprintf(message, "Could not open %s: %s\n", cur_str, strerror(errno));
+        die(message, __LINE__, __FILE__);
+      }
+      fwrite(line, sizeof(line), 1, r_file);
+      fclose(r_file);
+      lines_done++;
+      if (lines_done == lines_p_proc[current_rank]) current_rank++;
+      printf("line");
+    }
+  }
+
+
+
+
+  for (j = VEI_START; j < 3; j++)// VEI_END+1; j++)
+  {
+    free(veis[j-VEI_START].filename);
+  }
+
+
+}
+
 
 
 void initialise(int* nprocs, int* rank, int *size, double* tic)
@@ -105,6 +231,9 @@ void initialise(int* nprocs, int* rank, int *size, double* tic)
   MPI_Comm_rank(MPI_COMM_WORLD, rank);
   printf("Number of processes: %d, rank: %d\n", *nprocs, *rank);
   MPI_Status status;
+
+  assignLines(nprocs, rank, size, tic);
+/*
   int err;
 
   struct timeval timstr;
@@ -141,7 +270,7 @@ void initialise(int* nprocs, int* rank, int *size, double* tic)
     char t2_file[] = "t2.txt";
 
     // array of fps to prevent reopening?
-    /*
+    
     for (int j = VEI_START; j < VEI_END+1; j++)
     {
       char vei[6];
@@ -174,7 +303,7 @@ void initialise(int* nprocs, int* rank, int *size, double* tic)
       fclose(fid);
       free(fullfile);
     }
-	*/
+	
 
     *bcast_buffer = line_per_rank;
     printf("bcast_buffer per VEI per rank: %d\n", *bcast_buffer);
@@ -190,6 +319,7 @@ void initialise(int* nprocs, int* rank, int *size, double* tic)
   checkError(err, "broadcasting amount of lines", *rank, __LINE__);
 
   if (*rank != *nprocs-1) *size = *bcast_buffer;
+  */
 }
 
 void finalise(double* tic, int* rank)
@@ -226,6 +356,13 @@ void checkError(const int err, const char *op, const int rank, const int line)
     fflush(stderr);
     exit(EXIT_FAILURE);
   }
+}
+
+void die(const char* message, const int line, const char* file)
+{
+  fprintf(stderr, "Error at line %d of file %s:\n%s\n", line, file, message);
+  fflush(stderr);
+  exit(EXIT_FAILURE);
 }
 
 
